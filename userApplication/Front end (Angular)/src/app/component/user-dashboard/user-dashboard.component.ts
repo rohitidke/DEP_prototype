365667ad38a1d72d32b7a73cc512e5632b792c38
rohitidke <rtrohittidke3@gmail.com>
2023-05-08 17:45:12 +0200

    added germany map into the dashboard

--- userApplication/Front end (Angular)/src/app/component/user-dashboard/user-dashboard.component.ts
+++ userApplication/Front end (Angular)/src/app/component/user-dashboard/user-dashboard.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import * as Highcharts from 'highcharts';
-import HC_more from "highcharts/highcharts-more";
-HC_more(Highcharts);
+// import * as Highcharts from 'highcharts';
+// import HC_more from "highcharts/highcharts-more";
+// HC_more(Highcharts);
+import * as Highcharts from 'highcharts/highmaps';
+import * as worldMap from "@highcharts/map-collection/custom/world.geo.json";
+// import * as germanyMap from '@highcharts/map-collection/countries/de/de-all.geo.json';
+import * as germanyMap from 'src/assets/germany.json';
 
 import { SearchService } from 'src/app/service/search.service';
 
@@ -13,13 +17,15 @@ import { SearchService } from 'src/app/service/search.service';
   styleUrls: ['./user-dashboard.component.css']
 })
 export class UserDashboardComponent implements OnInit {
-  Highcharts = Highcharts
-  chartData = null
+  // Highcharts = Highcharts
+  Highcharts: typeof Highcharts = Highcharts;
   showClusters = false
   updateFlag = false
   showError = false
   oneToOneFlag = false
   loading = false
+  chartConstructor = 'mapChart';
+  chartData = [{ code3: "ABW", z: 105 }, { code3: "AFG", z: 35530 }];
 
   linechart: any = {
     series: [
@@ -93,5 +99,63 @@ export class UserDashboardComponent implements OnInit {
     localStorage.removeItem("token");
     this.route.navigate(['/']);
   }
+
+  chartOptions: Highcharts.Options = {
+    chart: {
+      map: germanyMap
+    },
+    title: {
+      text: 'Germany Index'
+    },
+    subtitle: {
+      text:
+        'Dummy data'
+    },
+    accessibility: {
+      typeDescription: 'Map of Germany.'
+    },
+    mapNavigation: {
+      enabled: true,
+      buttonOptions: {
+        verticalAlign: 'bottom'
+      }
+    },
+    legend: {
+      enabled: true
+    },
+    colorAxis: {
+      tickPixelInterval: 100
+    },
+    series: [
+      {
+        name: 'Random data',
+        type: 'map',
+        dataLabels: {
+          enabled: true,
+          format: '{point.properties.postal-code}'
+        },
+        keys: ['hasc', 'value'],
+        joinBy: 'hasc',
+        data:  [
+          ['DE.SH', 728],
+          ['DE.BE', 710],
+          ['DE.MV', 963],
+          ['DE.HB', 541],
+          ['DE.HH', 622],
+          ['DE.RP', 866],
+          ['DE.SL', 398],
+          ['DE.BY', 785],
+          ['DE.SN', 223],
+          ['DE.ST', 605],
+          ['DE.NW', 237],
+          ['DE.BW', 157],
+          ['DE.HE', 134],
+          ['DE.NI', 136],
+          ['DE.TH', 704],
+          ['DE.', 361]
+      ],
+      }
+    ]
+  };
 
 }
